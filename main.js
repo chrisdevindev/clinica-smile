1873a1bad2593c95937d6d18a20f94615d90ad20
CCastro01 <alvescastro00cca13@gmail.com>
2021-11-21 01:22:50 -0300

    Adding Acroll Reveal

--- main.js
+++ main.js
@@ -43,3 +43,17 @@ const swiper = new Swiper('.swiper', {
 })
 
 
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(`
+    #home .text, #home .image,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+    `, { interval:100 })
