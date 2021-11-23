b5aa3d132de1441b6fd43470730b2c7afcb909a6
CCastro01 <alvescastro00cca13@gmail.com>
2021-11-23 00:01:06 -0300

    Adding footer and scroll functions

--- main.js
+++ main.js
@@ -2,58 +2,71 @@ const nav = document.querySelector("#header nav");
 const toggle = document.querySelectorAll("nav .toggle");
 
 for (const element of toggle) {
-    element.addEventListener('click', () => {
-        nav.classList.toggle('show')
-    })
+  element.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
 }
 
-
-
-const links = document.querySelectorAll("nav ul li a")
+const links = document.querySelectorAll("nav ul li a");
 
 for (const link of links) {
-    link.addEventListener('click', () => {
-        nav.classList.remove('show')
-    })
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
+  });
 }
 
-
-
-
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', () => {
-    if(window.scrollY >= navHeight){
-        header.classList.add('scroll')
-    }
-    else{
-        header.classList.remove('scroll')
-    }
-})
-
-
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    pagination: {
-        el: '.swiper-pagination'
-    },
-    keyboard: true,
-
-})
-
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  keyboard: true,
+});
 
 const scrollReveal = ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 700,
-    reset: true
-})
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
 
-scrollReveal.reveal(`
+scrollReveal.reveal(
+  `
     #home .text, #home .image,
     #about .image, #about .text,
     #services header, #services .card,
     #testimonials header, #testimonials .testimonials,
-    #contact .text, #contact .links
-    `, { interval:100 })
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+    `,
+  { interval: 100 }
+);
+
+
+
+function changeHeaderWhenScroll() {
+    const header = document.querySelector("#header");
+    const navHeight = header.offsetHeight;
+  
+    if (window.scrollY >= navHeight) {
+      header.classList.add("scroll");
+    } else {
+      header.classList.remove("scroll");
+    }
+  }
+ 
+function backToTop(){
+    const backToTopButton = document.querySelector(".back-to-top");
+
+    if (window.scrollY >= 560) {
+      backToTopButton.classList.add("show");
+    } else {
+      backToTopButton.classList.remove("show");
+    }
+}
+
+window.addEventListener("scroll", () => {
+    changeHeaderWhenScroll();
+    backToTop();
+  });
+  
